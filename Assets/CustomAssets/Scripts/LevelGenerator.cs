b293d5a986bcf18f8a8d233acfaa8d7443fb94a4
KontoSenpai <senechal.jimmy@gmail.com>
2016-09-30 02:40:47 -0400

    Some hardwork on the procedural stuff

--- Assets/CustomAssets/Scripts/LevelGenerator.cs
+++ Assets/CustomAssets/Scripts/LevelGenerator.cs
@@ -25,7 +25,7 @@ public class LevelGenerator : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        InitializeTables();
+       InitializeTables();
        CompleteLevel();
     }
 	
@@ -56,35 +56,45 @@ public class LevelGenerator : MonoBehaviour
 
     }
 
-
-
     private bool CompleteLevel()
     {
+        // Know if the process of adding a block ended with 
+        bool blockFit = false;
+        bool rejected = false;
 
-        List<GameObject> blockTried;
-        blockTried = new List<GameObject>();
+        //The position in the scene( and in the grid) requested
+        Vector3 desiredPos = new Vector3();
 
+        //Control of attempts to get the fitting block
         int desiredBlock = -1;
-
-        bool good = false;
-        desiredBlock = Random.Range(0, 4);
-        //Si on à un bloc qui est bon ou pas
-        /*while( !good)
-        {
-            for( int i = 0; i < blockTried.Count-1; i++)
-            {
-                if (blockTried[i] = shapes[desiredBlock])
-                    good = false;
-                else
-                    good = true;
-            }
-        }*/
-        blockTried.Add(shapes[desiredBlock]);
+        List<int> indexTried = new List<int>();
+        
 
         if (placedShapes < numberOfShapes)
         {
-            GameObject platform = Instantiate(shapes[desiredBlock]) as GameObject;
-            platform.transform.position = new Vector3(Random.Range(2, 20), Random.Range(2, 20), 0);
+            do
+            {
+                rejected = false;
+                do
+                {
+                    print("0.Creating a vector");
+                    desiredPos = new Vector3(Random.Range(2, 17), Random.Range(5, 12), 0);
+                }
+                while (indexes[(int)desiredPos.x][(int)desiredPos.y] != 0);
+                // Now we gonna attemp to insertBlocks in the grid and thus, world space
+                do
+                {
+                    desiredBlock = GetRandomNumberWithExclusion(indexTried);
+                    blockFit = BlockFit(shapes[desiredBlock], desiredPos);
+                    if ( !blockFit && indexTried.Count == shapes.Count)
+                    {
+                        print("0..I don't fit :(");
+                        rejected = true;
+                    }
+                }
+                while (!blockFit || rejected);
+            }
+            while (rejected);
             placedShapes++;
             CompleteLevel();
         }
@@ -92,4 +102,93 @@ public class LevelGenerator : MonoBehaviour
             return true;
         return false;
     }
+
+    /** Function to tell weither it's possible or not to place a block on the grid
+    *
+    * @Params shape : selected shape for the placement
+    * @Params Ref : vector containin selection's root on the grid.
+    */
+    private bool BlockFit(GameObject shape, Vector3 refVector)
+    {
+        print("2.Testing if the selected block fit");
+        GameObject platform = Instantiate(shape) as GameObject;
+        List<List<int>> platformCollisions = platform.GetComponent<Shape>().GetCollision();
+        for( int i = 0; i < platformCollisions.Count; i++)
+        {
+            List<int> grood = platformCollisions[i];
+            for(int j = 0; j < grood.Count; j++)
+            {
+                if ( (indexes[(int)refVector.x + i][(int)refVector.y + j]) == 1 || (indexes[(int)refVector.x + i][(int)refVector.y + j]) == 2)
+                {
+                    print("2..It does not fit");
+                    Destroy(platform);
+                    return false;
+                }
+            }
+        }
+        FillTheGrid( platformCollisions, refVector);
+        platform.transform.position = refVector;
+        return true;
+    }
+
+    //UTILS
+    //
+    // Functions that help to structure and read the possibilities
+    //
+    //
+    private int GetRandomNumberWithExclusion(List<int> alreadyTried)
+    {
+        print("1.Getting a random value");
+        int desiredBlock = Random.Range(0, 4);
+        List<int> toasted = new List<int>();
+        bool noBlock = false;
+
+        if (alreadyTried.Count == 0)
+            return desiredBlock;
+        else
+        {
+            do
+            {
+                for (int i = 0; i < alreadyTried.Count; i++)
+                {
+                    if (desiredBlock == alreadyTried[i])
+                    {
+                        print("1..Value already tested");
+                        break;
+                    }
+                    else if(i == alreadyTried.Count)
+                    {
+                        print("1..No similar value was found");
+                        return desiredBlock;
+                    }
+                }
+                toasted.Add(desiredBlock);
+                if (toasted.Count == alreadyTried.Count)
+                {
+                    print("1...Tried all possibilities, canceling to try a new possition on the grid");
+                    noBlock = true;
+                }
+                else
+                {
+                    print("1...Trying a new number");
+                    desiredBlock = Random.Range(0, 4);
+                }
+            }
+            while (!noBlock);
+        }
+        return -1;
+    }
+
+    private void FillTheGrid(List<List<int>> objPos, Vector3 refVector)
+    {
+        for (int i = 0; i < objPos.Count; i++)
+        {
+            List<int> grood = objPos[i];
+            for (int j = 0; j < grood.Count; j++)
+            {
+                indexes[(int)refVector.x + i][(int)refVector.y + j] = grood[j];
+            }
+        }
+    }
+
 }
