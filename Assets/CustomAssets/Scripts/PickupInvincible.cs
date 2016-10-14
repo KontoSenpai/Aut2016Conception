57c39d65d2ab01085ac956a402e8cd3d4e0c1e64
Benjamin Urtado <urtado.benjamin@gmail.com>
2016-10-14 18:22:13 -0400

    Rename scrips pick up / add ossature script pickup invincible

---
+++ Assets/CustomAssets/Scripts/PickupInvincible.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupInvincible : MonoBehaviour {
+
+    // public AudioClip pickupSound;
+    // private AudioSource source;
+
+    public float activeTime = 2;
+    //public float volumeRange = 1f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //source = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // source.PlayOneShot(pickupSound, volumeRange);
+
+        }
+
+    }
+
+    private IEnumerator wait(Collider2D player, float maxSpeed)
+    {
+
+        yield return new WaitForSeconds(activeTime);
+    
+        Destroy(gameObject);
+    }
+
+
+
+}
