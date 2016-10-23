0e796334ac178a3a01fd04ca5f2a7becddf15bf7
Warren Benyahia <Warren@MBP-de-Warren.local>
2016-10-22 22:07:37 -0400

    ADD : menu basique et chargement du level

---
+++ Assets/CustomAssets/Scripts/LoadOnClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public GameObject loadingImage;
+
+	// Use this for initialization
+	public void LoadScene (int level) {
+		loadingImage.SetActive (true);
+		SceneManager.LoadScene(level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
