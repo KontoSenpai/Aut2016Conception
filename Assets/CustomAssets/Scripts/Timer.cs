5132f0ded618e85a9ffc0ba31f882283081356f8
Benjamin Urtado <urtado.benjamin@gmail.com>
2016-11-03 15:04:35 -0400

    Timer add
    Add timer script and screen view, need to do somethink when the timer it 0

---
+++ Assets/CustomAssets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float time;
+
+    public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+            //do some shit for ending/ continue game
+            time = 0;
+        }
+
+        int minute = (int) time / 60;
+        var seconds = time % 60;
+
+
+        timerText.text = string.Format("{0:00} : {1:00}", minute, seconds);
+        Debug.Log(string.Format("{0:00} : {1:00}", minute, seconds));
+
+       
+	
+	}
+}
