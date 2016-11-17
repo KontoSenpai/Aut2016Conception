69482ba28b441d9d391749878ee4b484841407eb
KontoSenpai <senechal.jimmy@gmail.com>
2016-11-17 03:42:01 -0500

    Potential final Jump behavior... Some hitbox changes

--- Assets/CustomAssets/Scripts/Player/GroundCollision.cs
+++ Assets/CustomAssets/Scripts/Player/GroundCollision.cs
@@ -10,13 +10,11 @@ public class GroundCollision : MonoBehaviour {
     float delay = 0.15f;
 
     Rigidbody2D rb;
-    Animator anim;
 
     // Use this for initialization
     void Start ()
     {
         rb = GetComponentInParent<Rigidbody2D>();
-        anim = GetComponentInParent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -25,11 +23,8 @@ public class GroundCollision : MonoBehaviour {
         if(JumpOccurs())
         {
             timeCollisions.Clear();
-            Vector3 tmp = rb.velocity;
-            tmp.y = 0.0f;
-            rb.velocity = tmp;
-            rb.AddForce(Vector2.up * jumpForce);
-            anim.SetBool("Ground", false);
+            GetComponentInParent<PlayerController>().Jump(jumpForce);
+            GetComponentInParent<PlayerController>().SetAnimation("Ground", false);
         }
     }
 
@@ -51,13 +46,11 @@ public class GroundCollision : MonoBehaviour {
 		if (col.gameObject.tag == "Ground")
         {
             if (timeCollisions.Count == 0)
-                anim.SetBool("Ground", true);
+                GetComponentInParent<PlayerController>().SetAnimation("Ground", true);
             timeCollisions.Add(Time.time);
         }
         else if(col.gameObject.tag == "Slider")
-        {
-            rb.AddForce(Vector2.up * slideForce);
-        }
+            GetComponentInParent<PlayerController>().Jump(slideForce);
     }
 
     void OnCollisionExit2D(Collision2D col)
--- Assets/CustomAssets/Scripts/Player/PlayerController.cs
+++ Assets/CustomAssets/Scripts/Player/PlayerController.cs
@@ -9,10 +9,12 @@ public class PlayerController : MonoBehaviour {
     private bool facingRight = true;
 	private Rigidbody2D rb;
     private int playerID;
+    Animator animator;
 
-	void Start () 
+    void Start () 
 	{
 		rb = GetComponent<Rigidbody2D> ();
+        animator = GetComponent<Animator>();
 		playerID = gameObject.GetComponent<PlayerStatus> ().GetID ();
 	}
 
@@ -58,6 +60,19 @@ public class PlayerController : MonoBehaviour {
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
+
+    public void SetAnimation(string param, bool value)
+    {
+        animator.SetBool(param, value);
+    }
+
+    public void Jump( float force)
+    {
+        Vector3 tmp = rb.velocity;
+        tmp.y = 0.0f;
+        rb.velocity = tmp;
+        rb.AddForce(Vector2.up * force);
+    }
 
     public float GetMaxSpeed() { return maxSpeed;}
     public void SetMaxSpeed(float max) { maxSpeed = max; }
