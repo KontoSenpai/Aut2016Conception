dd360cc47a8101089635ffd0689660d8c6092871
Warren Benyahia <Warren@MBP-de-Warren.local>
2016-11-25 15:45:52 -0500

    UPDATE : UI modified

--- Assets/CustomAssets/Scripts/HUD.cs
+++ Assets/CustomAssets/Scripts/HUD.cs
@@ -11,10 +11,12 @@ public class HUD : MonoBehaviour {
 	private int[] playerPreviousLife; 		//Array of players's current life
 	private int[] playerMaxLife; 			//Array of players's current life
 
-	private int heartPosXP1 = 50;			//Position X of Player 1 spawn of hearts
-	private int heartPosXP2 = 1150;			//Position X of Player 2 spawn of hearts
-	private int heartPosY = -100;			//Position Y of Players spawn of hearts
-	private int threshold = 100;			//Space between hearts
+	private int threshold = 75;			//Space between hearts
+	//Position X of Player 1 spawn of hearts (center screen - (distance between hearts + timer.length/2)
+	private int heartPosXP1 = (Screen.width/2)-(75*4) ;			
+	private int heartPosXP2 = (Screen.width/2)+(75*2);			//Position X of Player 2 spawn of hearts
+	private int heartPosY = Screen.height/2-100;			//Position Y of Players spawn of hearts
+
 
 	private List<GameObject> heartsListP1;	//List of Player 1 hearts currently shown
 	private List<GameObject> heartsListP2;	//List of Player 2 hearts currently shown
@@ -30,8 +32,8 @@ public class HUD : MonoBehaviour {
     public void Awake()
     {
       
-        canvasHight = canvas.GetComponent<RectTransform>().rect.height;
-        canvasWidth = canvas.GetComponent<RectTransform>().rect.width;
+		canvasHight = canvas.GetComponent<RectTransform>().rect.height;
+		canvasWidth = Screen.width/2;//canvas.GetComponent<RectTransform>().rect.width;
 
         heartsListP1 = new List<GameObject>();
         heartsListP2 = new List<GameObject>();
